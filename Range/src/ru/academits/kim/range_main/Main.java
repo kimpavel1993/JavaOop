791fc176c6e4123df223b2ef80958edc83ec5810
kimpavel1993 <kimpavel1993@mail.ru>
2020-02-20 02:15:50 +0700

    Add IDEA Project

--- Range/src/ru/academits/kim/range_main/Main.java
+++ Range/src/ru/academits/kim/range_main/Main.java
@@ -2,7 +2,7 @@ package ru.academits.kim.range_main;
 
 import ru.academits.kim.range.Range;
 
-public class    Main {
+public class Main {
     public static void main(String[] args) {
         Range range = new Range(1, 3);
         range.print();
