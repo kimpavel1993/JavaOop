0e810eb9d87048a550c5003e848684f1024280f9
kimpavel1993 <kimpavel1993@mail.ru>
2020-02-19 22:26:59 +0700

    Add IDEA Project

--- Range/src/ru/academits/kim/range/Range.java
+++ Range/src/ru/academits/kim/range/Range.java
@@ -1,4 +1,23 @@
 package ru.academits.kim.range;
 
 public class Range {
+    private double from;
+    private double to;
+
+    public Range(double from, double to) {
+        this.from = from;
+        this.to = to;
+    }
+
+    public double getLength() {
+        return to - from;
+    }
+
+    public boolean isInside(double number) {
+        return (number >= from) && (number <= to);
+    }
+
+    public void print() {
+        System.out.printf("(%s, %s)%n", from, to);
+    }
 }
--- Range/src/ru/academits/kim/range_main/Main.java
+++ Range/src/ru/academits/kim/range_main/Main.java
@@ -1,4 +1,13 @@
 package ru.academits.kim.range_main;
 
-public class Main {
+import ru.academits.kim.range.Range;
+
+public class    Main {
+    public static void main(String[] args) {
+        Range range = new Range(1, 3);
+        range.print();
+
+        System.out.println("Попадание в числовой диапазон: " + range.isInside(2));
+        System.out.println("Длина диапазона: " + range.getLength());
+    }
 }
