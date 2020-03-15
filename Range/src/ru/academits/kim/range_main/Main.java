76085199ddbbdb43db319b2e2304c9d72bc16579
kimpavel1993 <kimpavel1993@mail.ru>
2020-03-16 01:26:29 +0700

    Add IDEA Project

--- Range/src/ru/academits/kim/range_main/Main.java
+++ Range/src/ru/academits/kim/range_main/Main.java
@@ -4,10 +4,64 @@ import ru.academits.kim.range.Range;
 
 public class Main {
     public static void main(String[] args) {
-        Range range = new Range(1, 3);
-        range.print();
+        Range firstRange = new Range(4, 5);
+        Range secondRange = new Range(3, 7);
 
-        System.out.println("Попадание в числовой диапазон: " + range.isInside(2));
-        System.out.println("Длина диапазона: " + range.getLength());
+        firstRange.print();
+        secondRange.print();
+
+        System.out.println("Пересечение:");
+
+        Range intersection = Range.getIntersection(firstRange, secondRange);
+        if (intersection != null) {
+            intersection.print();
+        } else {
+            System.out.println("null");
+        }
+
+        System.out.println("Объединение:");
+
+        Object union = Range.getUnion(firstRange, secondRange);
+        if (union instanceof Range) {
+            Range unionRange = (Range) union;
+            unionRange.print();
+        } else if (union instanceof Range[]) {
+            Range[] unionArrayRange = (Range[]) union;
+
+            for (int i = 0; i < unionArrayRange.length; i++) {
+                unionArrayRange[i].print();
+            }
+        } else {
+            System.out.println("null");
+        }
+
+        System.out.println("Разность:");
+
+        Object difference = Range.getDifference(firstRange, secondRange);
+        if (difference instanceof Range) {
+            Range differenceRange = (Range) difference;
+            differenceRange.print();
+        } else if (difference instanceof Range[]) {
+            Range[] differenceArrayRange = (Range[]) difference;
+
+            for (int i = 0; i < differenceArrayRange.length; i++) {
+                differenceArrayRange[i].print();
+            }
+        } else {
+            System.out.println("null");
+        }
+
+        System.out.println("Попадание в числовой диапазон: " + firstRange.isInside(2));
+        System.out.println("Длина диапазона: " + firstRange.getLength());
+
+        firstRange.setFrom(3);
+        System.out.println("Первое число нового диапазона: " + firstRange.getFrom());
+
+        firstRange.setTo(7);
+        System.out.println("Последнее число нового диапазона: " + firstRange.getTo());
+
+        System.out.printf("Новый диапазон: (%s, %s)%n", firstRange.getFrom(), firstRange.getTo());
+        System.out.println("Попадание в новый числовой диапазон: " + firstRange.isInside(6));
+        System.out.println("Длина нового диапазона: " + firstRange.getLength());
     }
 }
