bb4b83a5d1a5b7948a8847aed882e367492c7eda
kimpavel1993 <kimpavel1993@mail.ru>
2020-04-22 13:10:27 +0700

    fix Vector

--- Vector/src/ru/academits/kim/vector/Vector.java
+++ Vector/src/ru/academits/kim/vector/Vector.java
@@ -148,7 +148,7 @@ public class Vector {
             return false;
         }
 
-        if (o == null || o.getClass() != this.getClass()) {
+        if (o == null || o.getClass() != getClass()) {
             return false;
         }
 
