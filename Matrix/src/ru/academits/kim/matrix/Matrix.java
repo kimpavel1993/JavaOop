2d3af992977a7ddca4911239dfca94d1351f407c
kimpavel1993 <kimpavel1993@mail.ru>
2020-09-18 19:28:53 +0700

    add a new task Matrix

---
+++ Matrix/src/ru/academits/kim/matrix/Matrix.java
@@ -0,0 +1,192 @@
+package ru.academits.kim.matrix;
+
+import ru.academits.kim.vector.Vector;
+
+public class Matrix {
+    private Vector[] arrayVectors;
+
+    public Matrix(int n, int m) {
+        if (n <= 0 || m <= 0) {
+            throw new IllegalArgumentException("Неверные входные данные. Необходимо, чтобы размерность столбцов и строк была > 0. Размерность строк: " + n + "Размерность столбцов: " + m);
+        }
+        arrayVectors = new Vector[n];
+
+        for (int i = 0; i < n; i++) {
+            arrayVectors[i] = new Vector(m);
+        }
+    }
+
+    public Matrix(Matrix clone) {
+        arrayVectors = new Vector[clone.arrayVectors.length];
+
+        for (int i = 0; i < clone.arrayVectors.length; i++) {
+            arrayVectors[i] = new Vector(clone.arrayVectors[i]);
+        }
+    }
+
+    public Matrix(double[][] array) {
+        arrayVectors = new Vector[array.length];
+
+        int maxLength = 0;
+
+        for (double[] e : array) {
+            if (e.length > maxLength) {
+                maxLength = e.length;
+            }
+        }
+
+        for (int i = 0; i < array.length; i++) {
+            arrayVectors[i] = new Vector(maxLength, array[i]);
+        }
+    }
+
+    private double[] getArray(Vector vector) {
+        double[] array = new double[vector.getSize()];
+
+        for (int i = 0; i < vector.getSize(); i++) {
+            array[i] = vector.getComponentByIndex(i);
+        }
+
+        return array;
+    }
+
+    public Matrix(Vector[] vectors) {
+        arrayVectors = new Vector[vectors.length];
+
+        int maxLength = 0;
+
+        for (Vector e : vectors) {
+            if (getArray(e).length > maxLength) {
+                maxLength = getArray(e).length;
+            }
+        }
+
+        for (int i = 0; i < vectors.length; i++) {
+            arrayVectors[i] = new Vector(maxLength, getArray(vectors[i]));
+        }
+    }
+
+    public int getLinesNumber() {
+        return arrayVectors.length;
+    }
+
+    public int getColumnsNumber() {
+        return arrayVectors[0].getSize();
+    }
+
+    public Vector getLineByIndex(int index) {
+        if (index < 0 || index >= getLinesNumber()) {
+            throw new IndexOutOfBoundsException("Индекс не должен быть меньше 0, а так же равен или больше значения, равному " + arrayVectors.length + ". Текущее значение индекса:  " + index);
+        }
+
+        return new Vector(arrayVectors[index]);
+    }
+
+    private void checkVectorSize(Vector vector) {
+        if (vector.getSize() != getColumnsNumber()) {
+            throw new IllegalArgumentException("У вставляемого вектора некорректная длина. Текущая длина: " + vector.getSize() + "; Необходимая длина: " + getColumnsNumber());
+        }
+    }
+
+    public void setLineByIndex(int index, Vector vector) {
+        if (index < 0 || index >= arrayVectors.length) {
+            throw new IndexOutOfBoundsException("Индекс не должен быть меньше 0, а так же равен или больше значения, равному " + arrayVectors.length + ". Текущее значение индекса:  " + index);
+        }
+
+        checkVectorSize(vector);
+
+        arrayVectors[index] = new Vector(vector);
+    }
+
+    public Vector getColumnByIndex(int index) {
+        if (index < 0 || index >= getColumnsNumber()) {
+            throw new IndexOutOfBoundsException("Индекс не должен быть меньше 0, а так же равен или больше значения, равному " + arrayVectors.length + ". Текущее значение индекса:  " + index);
+        }
+
+        Vector vector = new Vector(getLinesNumber());
+
+        for (int i = 0; i < getLinesNumber(); i++) {
+            vector.setComponentByIndex(i, arrayVectors[i].getComponentByIndex(index));
+        }
+
+        return vector;
+    }
+
+    public void transpose() {
+        Vector[] newColumns = new Vector[getColumnsNumber()];
+
+        for (int i = 0; i < getColumnsNumber(); i++) {
+            newColumns[i] = this.getColumnByIndex(i);
+        }
+
+        arrayVectors = newColumns;
+    }
+
+    public void multiplyByScalar(double scalar) {
+        for (Vector e : arrayVectors) {
+            e.multiplyByScalar(scalar);
+        }
+    }
+
+    private void checkMatrixSize(Matrix matrix) {
+        if (matrix.getColumnsNumber() != getColumnsNumber() || matrix.getLinesNumber() != getLinesNumber()) {
+            throw new IllegalArgumentException("Некорректные размеры матриц. У первой матрицы размерность строк: " + getLinesNumber() + ", столбцов: " + getColumnsNumber() + ". У второй матрицы размерность строк: " + matrix.getLinesNumber() + ", столбцов: " + matrix.getColumnsNumber());
+        }
+    }
+
+    public Matrix add(Matrix matrix) {
+        checkMatrixSize(matrix);
+
+        for (int i = 0; i < getLinesNumber(); i++) {
+            arrayVectors[i].add(matrix.arrayVectors[i]);
+        }
+
+        return this;
+    }
+
+    public Matrix subtract(Matrix matrix) {
+        checkMatrixSize(matrix);
+
+        for (int i = 0; i < getLinesNumber(); i++) {
+            arrayVectors[i].subtract(matrix.arrayVectors[i]);
+        }
+
+        return this;
+    }
+
+    public static Matrix getSum(Matrix matrix1, Matrix matrix2) {
+        Matrix matrix = new Matrix(matrix1);
+
+        return matrix.add(matrix2);
+    }
+
+    public static Matrix getDifference(Matrix matrix1, Matrix matrix2) {
+        Matrix matrix = new Matrix(matrix1);
+
+        return matrix.subtract(matrix2);
+    }
+
+  /*  public static Matrix getMultiply(Matrix matrix1, Matrix matrix2) {
+        if (matrix1.getColumnsNumber() != matrix2.getLinesNumber()) {
+            throw new IllegalArgumentException("Размерность столбцов матрицы должна совпадать с размерность строк другой матрицы. У первой матрицы размерность столбцов: " + matrix1.getColumnsNumber() + ", размерность строк второй матрицы: " + matrix2.getLinesNumber());
+        }
+
+
+    }*/
+
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder("{");
+
+        for (Vector e : arrayVectors) {
+            stringBuilder.append(e).append(", ");
+        }
+
+        stringBuilder.delete(stringBuilder.length() - 2, stringBuilder.length());
+        stringBuilder.append("}");
+
+        return stringBuilder.toString();
+    }
+
+}
---
+++ Matrix/src/ru/academits/kim/matrix_main/Main.java
@@ -0,0 +1,65 @@
+package ru.academits.kim.matrix_main;
+
+import ru.academits.kim.matrix.Matrix;
+import ru.academits.kim.vector.Vector;
+
+public class Main {
+    public static void main(String[] args) {
+        Matrix matrix1 = new Matrix(3, 5);
+
+        System.out.println(matrix1);
+
+        double[][] array1 = {{1}, {4, 50}, {23, 4, 5, 6, 88}};
+
+        Matrix matrix2 = new Matrix(array1);
+
+        Matrix matrix3 = new Matrix(matrix2);
+
+        double[] vector = {1, 2, 3, 4, 5};
+
+        Vector vector1 = new Vector(vector);
+        Vector vector2 = new Vector(8, vector);
+        Vector vector3 = new Vector(3);
+
+        Vector[] arrayVectors = {vector1, vector2, vector3};
+
+        Matrix matrix4 = new Matrix(arrayVectors);
+
+        System.out.println(matrix4);
+
+        System.out.println("Количество строк в матрице: " + matrix4.getLinesNumber());
+        System.out.println("Количество столбцов в матрице: " + matrix4.getColumnsNumber());
+        System.out.println("Получение вектора-строки по индексу: " + matrix3.getLineByIndex(1));
+
+        matrix2.setLineByIndex(2, vector1);
+        System.out.println("Задание вектора-строки по индексу: " + matrix2);
+
+        System.out.println("Получение вектора-столбца по индексу: " + matrix2.getColumnByIndex(1));
+        System.out.println("Матрица до транспонирования: " + matrix2);
+
+        matrix2.transpose();
+        System.out.println("Транспонирование матрицы: " + matrix2);
+
+        matrix2.multiplyByScalar(2);
+        System.out.println("Умножение матрицы на скаляр: " + matrix2);
+
+        matrix2.add(matrix2);
+        System.out.println("Сложение матриц: " + matrix2);
+
+        double[][] array2 = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}, {10, 11, 12}, {13, 14, 15}};
+
+        Matrix matrix5 = new Matrix(array2);
+
+        matrix2.subtract(matrix5);
+        System.out.println("Разность матриц: " + matrix2);
+
+        System.out.println("Сложение матриц: " + Matrix.getSum(matrix2, matrix2));
+        System.out.println("Разность матриц: " + Matrix.getDifference(matrix2, matrix5));
+
+
+
+
+    }
+
+}
+
