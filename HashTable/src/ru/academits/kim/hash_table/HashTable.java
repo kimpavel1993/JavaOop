5c57b7d57f6f7863a22d65745c569f04f3029132
kimpavel1993 <kimpavel1993@mail.ru>
2020-10-02 23:53:56 +0700

    add a new task HashTable

---
+++ HashTable/src/ru/academits/kim/hash_table/HashTable.java
@@ -0,0 +1,304 @@
+package ru.academits.kim.hash_table;
+
+import java.util.*;
+
+public class HashTable<T> implements Collection<T> {
+    private final ArrayList<T>[] lists;
+    private int size;
+    private int modCount;
+
+    public HashTable(int capacity) {
+        if (capacity < 0) {
+            throw new IllegalArgumentException("Вместимость должна быть > 0. Текущее значение вместимости: " + capacity);
+        }
+
+        //noinspection unchecked
+        lists = new ArrayList[capacity];
+    }
+
+    public HashTable() {
+        //noinspection unchecked
+        lists = new ArrayList[10];
+    }
+
+    private int getIndex(Object o) {
+        if (o == null) {
+            return 0;
+        }
+
+        return Math.abs(o.hashCode() % lists.length);
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+
+    @Override
+    public boolean contains(Object o) {
+        int index = getIndex(o);
+
+        if (lists[index] == null) {
+            return false;
+        }
+
+        return lists[index].contains(o);
+    }
+
+    private class MyTableIterator implements Iterator<T> {
+        private int currentIndex = -1;
+        private final int currentModCount = modCount;
+        private int arrayIndex = 0;
+        private int listIndex = 0;
+
+        @Override
+        public boolean hasNext() {
+            return currentIndex + 1 < size();
+        }
+
+        @Override
+        public T next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException("Коллекция кончилась");
+            }
+
+            if (currentModCount != modCount) {
+                throw new ConcurrentModificationException("В коллекцию добавились/удалились элементы");
+            }
+
+            currentIndex++;
+
+            T element;
+
+            for (int i = arrayIndex; i < lists.length; i++) {
+                arrayIndex = i;
+
+                if (lists[i] != null & lists[i].size() > 0) {
+                    element = lists[i].get(listIndex);
+
+                    listIndex++;
+
+                    if (listIndex == lists[i].size()) {
+                        arrayIndex++;
+                        listIndex = 0;
+
+                    }
+
+                    return element;
+                }
+            }
+
+            return null;
+        }
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new MyTableIterator();
+    }
+
+    @Override
+    public Object[] toArray() {
+        Object[] array = new Object[size];
+
+        int i = 0;
+
+        for (T e : this) {
+            array[i] = e;
+
+            i++;
+        }
+
+        return array;
+    }
+
+    @Override
+    public <T1> T1[] toArray(T1[] a) {
+        if (a.length < size) {
+            //noinspection unchecked
+            return (T1[]) Arrays.copyOf(toArray(), size, a.getClass());
+        }
+
+        //noinspection SuspiciousSystemArraycopy
+        System.arraycopy(toArray(), 0, a, 0, size);
+
+        if (a.length > size) {
+            a[size] = null;
+        }
+
+        return a;
+    }
+
+    @Override
+    public boolean add(T t) {
+        int index = getIndex(t);
+
+        if (lists[index] == null) {
+            lists[index] = new ArrayList<>();
+        }
+
+        lists[index].add(t);
+
+        modCount++;
+        size++;
+
+        return true;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        int index = getIndex(o);
+
+        if (lists[index] == null) {
+            return false;
+        }
+
+        if (lists[index].remove(o)) {
+            modCount++;
+            size--;
+
+            return true;
+        }
+
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        for (Object e : c) {
+            if (!contains(e)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends T> c) {
+        if (c.size() == 0) {
+            return false;
+        }
+
+        for (T element : c) {
+            add(element);
+        }
+
+        return c.size() > 0;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        if (c.size() == 0) {
+            return false;
+        }
+
+        boolean isRemoved = false;
+
+        for (ArrayList<T> e : lists) {
+            if (e != null) {
+                int sizeBeforeRemove = e.size();
+
+
+                if (e.removeAll(c)) {
+                    isRemoved = true;
+
+                    size -= sizeBeforeRemove - e.size();
+                }
+            }
+        }
+
+        if (isRemoved) {
+            modCount++;
+        }
+
+        return isRemoved;
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        if (isEmpty()) {
+            return false;
+        }
+
+        if (c.size() == 0) {
+            clear();
+
+            return true;
+        }
+
+        boolean isRetained = false;
+
+        for (ArrayList<T> e : lists) {
+            if (e != null) {
+                int sizeBeforeRetain = e.size();
+
+                if (e.retainAll(c)) {
+                    isRetained = true;
+
+                    size -= sizeBeforeRetain - e.size();
+                }
+            }
+        }
+
+        if (isRetained) {
+            modCount++;
+        }
+
+        return isRetained;
+    }
+
+    @Override
+    public void clear() {
+        if (isEmpty()) {
+            return;
+        }
+
+        Arrays.fill(lists, null);
+
+        size = 0;
+        modCount++;
+    }
+
+    @Override
+    public String toString() {
+        if (size == 0) {
+            return "Пустая таблица";
+        }
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (ArrayList<T> e : lists) {
+            if (e != null && e.size() > 0) {
+                int i = 0;
+
+                for (T element : e) {
+                    int index = getIndex(element);
+
+                    if (i == 0) {
+                        String result = String.format("%d. %s, ", index, element);
+
+                        stringBuilder.append(result);
+
+                        i++;
+                    } else {
+                        String result = String.format("%s, ", element);
+
+                        stringBuilder.append(result);
+                    }
+                }
+
+                stringBuilder.delete(stringBuilder.length() - 2, stringBuilder.length());
+                stringBuilder.append(System.lineSeparator());
+            }
+        }
+
+        return stringBuilder.toString();
+    }
+}
---
+++ HashTable/src/ru/academits/kim/hash_table_main/Main.java
@@ -0,0 +1,74 @@
+package ru.academits.kim.hash_table_main;
+
+import ru.academits.kim.hash_table.HashTable;
+
+import java.util.ArrayList;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        HashTable<Integer> hashTable = new HashTable<>();
+        hashTable.add(100);
+        hashTable.add(101);
+        hashTable.add(102);
+        hashTable.add(103);
+        hashTable.add(104);
+        hashTable.add(105);
+        hashTable.add(106);
+        hashTable.add(107);
+        hashTable.add(108);
+        hashTable.add(109);
+
+        System.out.println(hashTable);
+
+        System.out.println("Наличие элемента в массиве списков: " + hashTable.contains(101));
+
+        Iterator<?> iterator = hashTable.iterator();
+
+        System.out.println("Список по элементам:");
+
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+        
+        System.out.println("Преобразование таблицы в массив: " + Arrays.toString(hashTable.toArray()));
+
+        Integer[] numbers = {34, 88, 92, 232, 542, 755, 23, 34, 666, 1, 2, 7, 5, 343};
+
+        System.out.println("Преобразование списка в массив с добавлением элементов массива: " + Arrays.toString(hashTable.toArray(numbers)));
+
+        ArrayList<Integer> numbersList2 = new ArrayList<>();
+
+        for (int i = 0; i < 100; i++) {
+            Random random = new Random();
+
+            numbersList2.add(random.nextInt(100) + 1);
+        }
+
+        hashTable.addAll(numbersList2);
+
+        System.out.println("Таблица с добавленными элементами:");
+        System.out.println(hashTable);
+
+        ArrayList<Integer> numbersList3= new ArrayList<>(Arrays.asList(100, 101,102, 103, 104, 105, 106, 107, 108, 109));
+
+        System.out.println("Наличие списка в списке: " + hashTable.containsAll(numbersList3));
+        System.out.println("Наличие удаленных элементов в списке: " + hashTable.removeAll(numbersList3));
+
+        System.out.println("Список с удаленными элементами:");
+        System.out.println(hashTable);
+
+        ArrayList<Integer> numbersList4 = new ArrayList<>(Arrays.asList(1, 2 ,3, 4, 5, 6, 7, 8, 9));
+
+        System.out.println("Сохранение только указанных элементов в списке: " + hashTable.retainAll(numbersList4));
+
+        System.out.println("Список только с указанными элементами:");
+        System.out.println(hashTable);
+
+        hashTable.clear();
+        System.out.println("Очищение списка: " + hashTable);
+    }
+}
